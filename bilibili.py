71268c3262b46828ab38f0df6de4faccb6411275
ho <takanashirikkax@gmail.com>
2018-08-01 22:06:57 +0800

    v0.0.2 增加登录和查看当前用户信息

--- bilibili.py
+++ bilibili.py
@@ -5,7 +5,41 @@ import requests
 class Bilibili:
     def __init__(self):
         self.session = requests.session()
+        self.csrf = None
 
+    def login(self, username, password):
+        """
+        调用第三方API，请谨慎使用
+        详情见：http://docs.kaaass.net/showdoc/web/#/2?page_id=3
+        :param username:登录的用户名
+        :param password:密码
+        :return:
+        """
+        req = self.post(
+            url='https://api.kaaass.net/biliapi/user/login',
+            data={
+                'user': username,
+                'passwd': password
+            }
+        )
+        if req['status'] == 'OK':
+            login = self.get(
+                url='https://api.kaaass.net/biliapi/user/sso',
+                params={
+                    'access_key': req['access_key']
+                }
+            )
+            if login['status'] == 'OK':
+                cookies = {}
+                for line in login['cookie'].split(';')[:-1]:
+                    name, value = line.strip().split('=')
+                    cookies[name] = value
+                cookies = requests.utils.cookiejar_from_dict(cookies, cookiejar=None, overwrite=True)
+                self.session.cookies = cookies
+                self.csrf = self.session.cookies.get('bili_jct')
+                return True
+            else:
+                return login
     def post(self, url, data, headers=None):
         while True:
             try:
@@ -48,3 +82,13 @@ class Bilibili:
                     sys.exit(0)
             except Exception as e:
                 print("GET出错\n%s" % str(e))
+
+    def user_info(self):
+        """
+        获取当前用户的信息
+        :return:
+        """
+        req = self.get(
+            url='https://member.bilibili.com/x/web/white'
+        )
+        return req
